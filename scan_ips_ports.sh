a889533793bd96b3af7146bdac03dd23f65fc1b4
zed2728393 <zed9992025@outlook.com>
2025-10-08 02:38:05 -0400

    Update scan_ips_ports.sh

--- scan_ips_ports.sh
+++ scan_ips_ports.sh
@@ -8,7 +8,7 @@ file_number="$2"
 sed -E 's~https?://~~' "$input_file" > live_hosts_final.txt
 
 # Run nmap service/version scan on live host
-sudo nmap -sV -sN -O -iL live_hosts_final.txt -oN "nmap_scan_$2.txt"
+sudo nmap -sV -sN -O -iL live_hosts_final.txt -oN "nmap_scan.txt"
 
 
 
